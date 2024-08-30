90b96e95a7a142a93e02a532212b818366283829
bukhorimukhammad <bukhorimukhammad@gmail.com>
2024-08-31 01:13:32 +0700

    Update fix.sh

--- Insshws/fix.sh
+++ Insshws/fix.sh
@@ -9,13 +9,13 @@ rm -rf /etc/systemd/system/edu-proxy.service
 rm -rf /etc/systemd/system/ws-dropbear.service
 rm -rf /etc/systemd/system/edu-proxyovpn
 
-curl "https://raw.githubusercontent.com/kanghory/VPN/main/Insshws/insshws.sh" | bash
+curl "https://raw.githubusercontent.com/bukhorimukhammad/VPN/main/Insshws/insshws.sh" | bash
 
 
 
-wget -O /usr/local/bin/edu-proxy https://raw.githubusercontent.com/kanghory/websocket/main/openssh.py && chmod +x /usr/local/bin/edu-proxy
+wget -O /usr/local/bin/edu-proxy https://raw.githubusercontent.com/bukhorimukhammad/VPN/main/Insshws/openssh.py && chmod +x /usr/local/bin/edu-proxy
 
-wget -O /etc/systemd/system/edu-proxy.service https://gitlab.com/hidessh/baru/-/raw/main/websocket-python/baru/http.service && chmod +x /etc/systemd/system/edu-proxy.service
+wget -O /etc/systemd/system/edu-proxy.service https://raw.githubusercontent.com/bukhorimukhammad/VPN/main/Insshws/http.service && chmod +x /etc/systemd/system/edu-proxy.service
 
 systemctl daemon-reload
 systemctl enable edu-proxy.service
