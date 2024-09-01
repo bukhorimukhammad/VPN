545fb9dbe27ac01233d1e9c97c4f74b520b2eea6
bukhorimukhammad <bukhorimukhammad@gmail.com>
2024-09-01 10:30:34 +0700

    Update senmenu.sh

--- allmenu/senmenu.sh
+++ allmenu/senmenu.sh
@@ -44,7 +44,7 @@ wget -q -O /root/versi "https://raw.githubusercontent.com/bukhorimukhammad/VPN/m
 #wget -q -O /usr/bin/updatsc "https://raw.githubusercontent.com/kanghory/VPN/main/update.sh"
 #wget -q -O /usr/bin/backup "https://raw.githubusercontent.com/kanghory/VPN/main/backup.sh"
 wget -q -O /usr/bin/renel "https://raw.githubusercontent.com/bukhorimukhammad/VPN/main/allmenu/renel.sh"
-wget -q -O /usr/bin/cek-log-vless "https://raw.githubusercontent.com/bukhorimukhammad/VPN/main/allmenu/cek-log-vless.sh"
+wget -q -O /usr/bin/cekl"https://raw.githubusercontent.com/bukhorimukhammad/VPN/main/allmenu/cekl.sh"
 wget -q -O /usr/bin/dell "https://raw.githubusercontent.com/bukhorimukhammad/VPN/main/allmenu/dell.sh"
 #wget -q -O /usr/bin/ns-menu "https://raw.githubusercontent.com/kanghory/VPN/main/autoscript-ssh-slowdns-main/slowdns.sh && chmod +x slowdns.sh && ./slowdns.sh"
 wget https://raw.githubusercontent.com/bukhorimukhammad/VPN/main/backup/set-br.sh
@@ -57,7 +57,7 @@ chmod +x /usr/bin/add-ssws
 chmod +x /usr/bin/add-vless
 chmod +x /usr/bin/add-tr
 chmod +x /usr/bin/addl
-chmod +x /usr/bin/cek-log-vless
+chmod +x /usr/bin/cekl
 chmod +x /usr/bin/dell
 chmod +x /usr/bin/renel
 chmod +x /usr/bin/usernew
